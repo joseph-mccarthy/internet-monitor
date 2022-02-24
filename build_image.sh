3937a996d07ce511659098b3275095194f24ba3d
Joseph McCarthy <joseph-mccarthy@users.noreply.github.com>
2022-02-24 18:04:37 +0000

    update docker image for slim build

--- build_image.sh
+++ build_image.sh
@@ -1,1 +1,1 @@
-sudo docker buildx build --platform linux/amd64,linux/arm/v7 -t joemccarthy/internet-monitor:latest --push .
+sudo docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm/v6 -t joemccarthy/internet-monitor:latest --push .
